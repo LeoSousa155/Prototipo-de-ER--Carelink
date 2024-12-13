c1a6ed3d438bb1c05f96debd192b1e977cef6a8c
wertsun <hhenriquesleonardo15@gmail.com>
2024-12-13 14:42:56 +0000

    db eventos
    Added events to the database

--- server/db.js
+++ server/db.js
@@ -12,7 +12,21 @@ function createTable() {
         age INTEGER
     ) STRICT
     `);
+    database.exec(`
+        CREATE TABLE IF NOT EXISTS Event(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            day INTEGER,
+            month INTEGER,
+            person_id INTEGER,
+            CONSTRAINT fk_event
+                FOREIGN KEY (person_id)
+                REFERENCES Person(id)
+        )
+        `);
 }
+
+
 
 function deleteTable() {
     // Execute SQL statements from strings.
@@ -27,15 +41,36 @@ function insertData(name, age) {
     insert.run(name, age);
 }
 
+function insertDataEvent(subject, day, month, pessoa_id) {
+    // Execute a SQL statement.
+    const insert = database.prepare(`INSERT INTO Event (subject, day, month, person_id) VALUES (?, ?, ?, ?)`);
+    insert.run(subject, day, month, pessoa_id);
+}
+
+
 function getAllData() {
     // Execute a SQL statement.
     const query = database.prepare(`SELECT * FROM Person ORDER BY age`);
     console.log(query.all());
 }
 
+function getAllDataEvent(person_id) {
+    // Execute a SQL statement.
+    const query = database.prepare(`SELECT * FROM Event WHERE person_id = ? GROUP BY month, day `);
+    query.run(person_id);
+    console.log(query.all(person_id));
+}
+
 console.log("Testando o banco de dados");
 createTable();
-insertData("João", 25);
-insertData("Maria", 30);
-insertData("Pedro", 20);
+//insertData("João", 25);
+//insertData("Maria", 30);
+//insertData("Pedro", 20);
 getAllData();
+//insertDataEvent("consulta1", 15, 9, 1);
+//insertDataEvent("consulta2", 10, 10, 2);
+//insertDataEvent("consulta3", 12, 6, 3);
+//insertDataEvent("consulta4", 16, 9, 1);
+getAllDataEvent(1);
+getAllDataEvent(2);
+getAllDataEvent(3);
